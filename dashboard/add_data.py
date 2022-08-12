d38f647f1d5608f20ddd72b1588f3ebacdfe0d46
Kiiru-Anastasia <kiiru.anastasia@gmail.com>
2022-08-12 10:03:52 +0300

    change data path

--- dashboard/add_data.py
+++ dashboard/add_data.py
@@ -211,6 +211,6 @@ if __name__ == "__main__":
     emojiDB(dbName='tweets')
     createTables(dbName='tweets')
 
-    df = pd.read_csv('.data/cleaned_fintech.csv')
+    df = pd.read_csv('../data/cleaned_fintech_data.csv')
 
     insert_to_tweet_table(dbName='tweets', df=df, table_name='TweetInformation')
